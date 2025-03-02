675b1b1c285e2f4e017a4cde6b1cfe343546d2ac
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-03-02 20:32:59 +0200

    add CORS support and improve API versioning in Express server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,44 @@
-import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+
+const allowedOrigins = [
+    "http://localhost:3000",
+];
+
+app.use(
+    cors({
+        origin: allowedOrigins,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+const version = process.env.API_VERSION!;
+const baseUrl = `${process.env.BASE_URL!}/${version}`;
+
+console.log(baseUrl);
+
+app.get(`${baseUrl}/`, (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.use("*", (req, res) => {
+    res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: "Route not found",
+    });
 });
