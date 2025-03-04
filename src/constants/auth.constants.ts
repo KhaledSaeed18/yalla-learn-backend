febbd5f992614a91499c4a18d1e0fb4bad62551c
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-03-04 13:01:20 +0200

    update blocked domains and common passwords for enhanced security

--- src/constants/auth.constants.ts
+++ src/constants/auth.constants.ts
@@ -1,13 +1,28 @@
 export const BLOCKED_DOMAINS = [
     'test.com',
     'example.com',
+    'localhost.com',
     'tempmail.com',
-    'throwaway.com'
+    'throwaway.com',
+    'mailinator.com',
+    'guerrillamail.com',
+    'yopmail.com',
+    'temp-mail.org',
+    'fakeinbox.com',
+    '10minutemail.com',
+    'trashmail.com',
+    'sharklasers.com',
+    'mailnesia.com',
+    'disposable.com',
+    'getnada.com',
+    'dispostable.com',
+    'maildrop.cc'
 ];
 
 export const COMMON_PASSWORDS = [
     'Password123!',
     'Admin123!',
+    'P@ssw0rd',
     'P@ssw0rd2024',
     'P@ssw0rd2025',
     'Qwerty123!',
@@ -15,10 +30,17 @@ export const COMMON_PASSWORDS = [
     'Secret123!',
     'Letmein123!',
     'ABC123abc!',
-    'Winter2025!',
-    'Summer2025!',
     '12345678Aa!',
     'Password1!',
     'Abcd1234!',
-    'Login1234!'
+    'Login1234!',
+    'Winter2025!',
+    'Summer2025!',
+    'Spring2025!',
+    'Fall2025!',
+    'Pa$$w0rd',
+    'Adm1n123!',
+    'L0g1n!123',
+    'W3lc0me!',
+    'Ch4ng3m3!'
 ];
